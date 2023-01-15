37ad6abeb2f3f8d67819e1019ca8fd5181191428
flavorbreaker <turner.l.anthony@gmail.com>
2023-01-14 20:22:56 -0500

    Removed unused packages.

--- app/src/main/java/com/example/composequadrant_codelab/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant_codelab/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.composequadrant_codelab
 
-import android.media.midi.MidiOutputPort
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -14,14 +11,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composequadrant_codelab.ui.theme.ComposeQuadrant_CodelabTheme
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 
 
 class MainActivity : ComponentActivity() {
